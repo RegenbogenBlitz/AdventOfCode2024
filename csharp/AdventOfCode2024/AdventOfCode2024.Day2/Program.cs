e90b37fa3c251849f0f6c5cc7c507ddbe60903f5
RegenbogenBlitz <37896982+RegenbogenBlitz@users.noreply.github.com>
2024-12-02 21:24:38 +0000

    refactor day 2 part 1 csharp

--- csharp/AdventOfCode2024/AdventOfCode2024.Day2/Program.cs
+++ csharp/AdventOfCode2024/AdventOfCode2024.Day2/Program.cs
@@ -2,46 +2,43 @@ namespace AdventOfCode2024.Day2;
 
 public static class Program
 {
-    public static int Part1(string filename)
+    public static int Part1(string filename) =>
+        File.ReadAllLines(filename)
+            .Select(line =>
+                line.Split(' ')
+                    .Select(int.Parse)
+                    .ToArray())
+            .Count(SequenceIsSafe);
+    
+    private static bool SequenceIsSafe(int[] numbers)
     {
-        var totalSafe = 0;
-        var lines = File.ReadAllLines(filename);
-        foreach (var line in lines)
+        var isSafe = true;
+        if(numbers.Length <= 1)
         {
-            var isSafe = true;
-            var numbers = line.Split(' ').Select(int.Parse).ToArray();
-            if(numbers.Length <= 1)
+            throw new NotImplementedException();
+        }
+        var isIncreasing = numbers[1] > numbers[0];
+        for (var i = 1; i < numbers.Length; i++)
+        {
+            var diff = Math.Abs(numbers[i] - numbers[i - 1]);
+            if(diff < 1 || diff > 3)
             {
-                throw new NotImplementedException();
+                isSafe = false;
+                break;
             }
-            var isIncreasing = numbers[1] > numbers[0];
-            for (var i = 1; i < numbers.Length; i++)
-            {
-                var diff = Math.Abs(numbers[i] - numbers[i - 1]);
-                if(diff < 1 || diff > 3)
-                {
-                    isSafe = false;
-                    break;
-                }
                 
-                if(isIncreasing && numbers[i] < numbers[i - 1])
-                {
-                    isSafe = false;
-                    break;
-                }
-                else if (!isIncreasing && numbers[i] > numbers[i - 1])
-                {
-                    isSafe = false;
-                    break;
-                }
-            }
-            
-            if(isSafe)
+            if(isIncreasing && numbers[i] < numbers[i - 1])
             {
-                totalSafe++;
+                isSafe = false;
+                break;
+            }
+            else if (!isIncreasing && numbers[i] > numbers[i - 1])
+            {
+                isSafe = false;
+                break;
             }
         }
 
-        return totalSafe;
+        return isSafe;
     }
 }
