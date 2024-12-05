a7c11e7f1cc2710a5ad435a20f8f0d2e58f99b4a
RegenbogenBlitz <37896982+RegenbogenBlitz@users.noreply.github.com>
2024-12-05 20:29:49 +0000

    solve day 5 part 2 csharp

--- csharp/AdventOfCode2024/AdventOfCode2024.Day5/Program.cs
+++ csharp/AdventOfCode2024/AdventOfCode2024.Day5/Program.cs
@@ -4,6 +4,53 @@ public static class Program
 {
     public static int Part1(string filename)
     {
+        var (_, passingLines, _) = PassingLines(filename);
+
+        var total = 0;
+        foreach (var pageNumbers in passingLines)
+        {
+            var pageCount = pageNumbers.Length;
+            var middleIndex = pageCount / 2;
+            total += pageNumbers[middleIndex];
+        }
+        return total;
+    }
+
+    public static int Part2(string filename)
+    {
+        var (rules, _, _failingLines) = PassingLines(filename);
+
+        var total = 0;
+        foreach (var failingLine in _failingLines)
+        {
+            failingLine.Sort(
+                (a, b) =>
+                {
+                    if (rules.ContainsKey(a) && rules[a].Contains(b))
+                    {
+                        return -1;
+                    }
+
+                    if (rules.ContainsKey(b) && rules[b].Contains(a))
+                    {
+                        return 1;
+                    }
+
+                    return 0;
+                });
+            
+            var asArray = failingLine.ToArray();
+            var pageCount = asArray.Length;
+            var middleIndex = pageCount / 2;
+            
+            total += asArray[middleIndex];
+        }
+        
+        return total;
+    }
+    
+    private static (Dictionary<int, List<int>> rules, List<int[]> passingLines, List<List<int>> failingLines) PassingLines(string filename)
+    {
         var lines = File.ReadAllLines(filename);
         var ruleTexts = lines.Where(l => l.Contains('|'));
         var pageTexts = lines.Where(l => l.Contains(','));
@@ -21,36 +68,40 @@ public static class Program
             }
             rules[first].Add(second);
         }
-
-        var total = 0;
+        
+        var passingLines = new List<int[]>();
+        var failingLines = new List<List<int>>();
+        
         foreach (var pageText in pageTexts)
         {
             var pageNumbers = pageText.Split(',').Select(p=>int.Parse(p)).ToList();
-            var pageCount = pageNumbers.Count;
-            var middleIndex = pageCount / 2;
-
+            
             var passes = true;
-            for(var i=0; i<pageNumbers.Count; i++)
+            for (var i = 0; i < pageNumbers.Count; i++)
             {
                 var pageNumber = pageNumbers[i];
                 if (rules.ContainsKey(pageNumber))
                 {
                     var previousPages = pageNumbers.Take(i).ToList();
-                    
-                    if (previousPages.Any(p=>rules[pageNumber].Contains(p)))
+
+                    if (previousPages.Any(p => rules[pageNumber].Contains(p)))
                     {
                         passes = false;
                         break;
                     }
                 }
             }
-            
+
             if (passes)
             {
-                total+= pageNumbers[middleIndex];
+                passingLines.Add(pageNumbers.ToArray());
+            }
+            else
+            {
+                failingLines.Add(pageNumbers);
             }
         }
-        
-        return total;
+
+        return (rules, passingLines, failingLines);
     }
 }
--- csharp/AdventOfCode2024/AdventOfCode2024.Day5/UnitTest1.cs
+++ csharp/AdventOfCode2024/AdventOfCode2024.Day5/UnitTest1.cs
@@ -17,4 +17,20 @@ public class Tests
 
         Assert.That(total, Is.EqualTo(6260));
     }
+    
+    [Test]
+    public void Part2Example()
+    {
+        var total = Program.Part2("example1.txt");
+        
+        Assert.That(total, Is.EqualTo(123));
+    }
+
+    [Test]
+    public void Part2Solution()
+    {
+        var total = Program.Part2("input.txt");
+
+        Assert.That(total, Is.EqualTo(5346));
+    }
 }
